265456d612c6706af0214a11b90a13d23b1a3eb9
yogamhd <yogapratamamhd@gmail.com>
2021-03-27 20:40:26 +0700

    Second Commit

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// referenced by https://github.com/expressjs/express/blob/e1b45ebd050b6f06aa38cda5aaf0c21708b0c71e/lib/application.js
+// and https://github.com/mysqljs/mysql/blob/master/lib/Connection.js
+
 const app = require('express').application;
 const mysql = require('mysql');
 const http = require('http');
@@ -6,11 +9,13 @@ const res = require('express').response;
 mysql.originalCreateConnection = mysql.createConnection;
 res.originalRedirect = res.redirect;
 
+// app.listen を上書き
 app.listen = function listen(port) {
   var server = http.createServer(this);
   return server.listen.apply(server, [3000]);
 };
 
+// createConnection を上書き
 mysql.createConnection = function createConnection(config) {
   var _this = this;
   if (process.env.IDENTIFIER === 'ANSWER'){
@@ -19,6 +24,7 @@ mysql.createConnection = function createConnection(config) {
   return mysql.originalCreateConnection.call(_this, config);
 }
 
+// res.redirect を上書き
 res.redirect = function redirect(url) {
   var _this = this;
   new_url = url + "?containerPort=3000&languageName=nodejs";
